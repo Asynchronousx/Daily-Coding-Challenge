8e064f29ce8a25b65e839f014618079e9011efeb
Asynchronousx <38207760+Asynchronousx@users.noreply.github.com>
2019-01-08 00:07:22 +0100

    Create Annotations.java

---
+++ Two List Intersection/src/Annotations.java
@@ -0,0 +1,7 @@
+//Useful annotation to take track of the code section.
+@interface Attributes { String value() default ""; }
+@interface Constructors { String value() default ""; }
+@interface AccessModifiers { String value() default ""; }
+@interface Methods { String value() default ""; }
+
+
