246f41c79a1c1f85f0bbdcecf77be95d5b63dec7
Asynchronousx <38207760+Asynchronousx@users.noreply.github.com>
2019-01-11 21:37:15 +0100

    Create Annotations.java

---
+++ Unival Tree Count/src/Annotations.java
@@ -0,0 +1,6 @@
+//Useful annotation to take track of the code section.
+@interface Attributes { String value() default ""; }
+@interface Constructors { String value() default ""; }
+@interface AccessModifiers { String value() default ""; }
+@interface Methods { String value() default ""; }
+@interface HelperMethod { String value() default ""; }
